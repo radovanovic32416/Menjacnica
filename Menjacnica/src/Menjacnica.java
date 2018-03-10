c7c3cf05c54df698788b2ec89d39b9b3b74c3ec5
radovanovic32416 <radovanovic32416@fon.bg.ac.rs>
2018-03-10 15:04:24 +0100

    Dodata klasa Menjacnica, koja implementira interfejs, nefunkcionalna

---
+++ Menjacnica/src/Menjacnica.java
@@ -0,0 +1,20 @@
+import java.util.GregorianCalendar;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(double prodajni, double kupovni, double srednji, GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public void obrisiKurs(GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public Kurs vratiKurs(GregorianCalendar datum) {
+		return null;
+	}
+
+}
