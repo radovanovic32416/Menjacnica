bf84117ab1bdf04521b21a4c45fd2bc2191aa536
radovanovic32416 <radovanovic32416@fon.bg.ac.rs>
2018-03-10 15:12:50 +0100

    Metode klase Menjacnica implementirane do kraja

--- Menjacnica/src/Menjacnica.java
+++ Menjacnica/src/Menjacnica.java
@@ -1,20 +1,42 @@
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+
 	@Override
 	public void dodajKurs(double prodajni, double kupovni, double srednji, GregorianCalendar datum) {
+		Kurs novi = new Kurs();
+		novi.setKupovni(kupovni);
+		novi.setProdajni(prodajni);
+		novi.setSrednji(srednji);
 
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getDatum().equals(datum))
+				valute.get(i).setKurs(novi);
+		}
 	}
 
 	@Override
 	public void obrisiKurs(GregorianCalendar datum) {
-
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getDatum().equals(datum))
+				valute.get(i).setKurs(null);
+		}
 	}
 
 	@Override
 	public Kurs vratiKurs(GregorianCalendar datum) {
-		return null;
+		Kurs trazeni = new Kurs();
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getDatum().equals(datum)) {
+				trazeni.setKupovni(valute.get(i).getKurs().getKupovni());
+				trazeni.setProdajni(valute.get(i).getKurs().getProdajni());
+				trazeni.setSrednji(valute.get(i).getKurs().getSrednji());
+			}
+		}
+		return trazeni;
 	}
 
 }
